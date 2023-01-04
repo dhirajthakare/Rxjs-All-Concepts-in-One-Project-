1966f5cd64d100f4e94e19bb8df927ae68168592
DhirajLucidux <88828651+DhirajLucidux@users.noreply.github.com>
2023-01-04 15:17:48 +0530

    updated version

--- src/app/service/httpservice.service.ts
+++ src/app/service/httpservice.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoadingBarService } from '@ngx-loading-bar/core';
 import { Observable } from 'rxjs';
--- src/app/service/middlewareservice.service.ts
+++ src/app/service/middlewareservice.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AsyncSubject, BehaviorSubject, Observable, ReplaySubject, Subject } from 'rxjs';
 
