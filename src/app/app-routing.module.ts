840aea9f3f93e43917bb644fa3b52119be3d1883
DhirajLucidux <88828651+DhirajLucidux@users.noreply.github.com>
2023-01-06 17:28:06 +0530

    added pipes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,9 @@ const routes: Routes = [
   { path: 'operator', loadChildren: () => import('./module/rxjs-operator/rxjs-operator.module').then(m => m.RxjsOperatorModule) },
   { path: 'observable', loadChildren: () => import('./module/observable/observable.module').then(m => m.ObservableModule) }, { path: 'navbar', loadChildren: () => import('./common/navbar/navbar.module').then(m => m.NavbarModule) },
   { path: 'subject', loadChildren: () => import('./module/subject/subject.module').then(m => m.SubjectModule) },
-  { path: 'forms', loadChildren: () => import('./module/forms/forms.module').then(m => m.FormssModule) }];
+  { path: 'forms', loadChildren: () => import('./module/forms/forms.module').then(m => m.FormssModule) },
+  { path: 'pipes', loadChildren: () => import('./module/pipes-exp/pipes-exp.module').then(m => m.PipesExpModule) },
+  { path: 'looks', loadChildren: () => import('./module/look-cycle/look-cycle.module').then(m => m.LookCycleModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
