03bdac7e9c33dd7eb7cdc7b22ce8b7b650ff2ec0
DhirajLucidux <88828651+DhirajLucidux@users.noreply.github.com>
2023-01-09 14:40:53 +0530

    updated code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoadingBarModule } from '@ngx-loading-bar/core';
 import { NavbarModule } from './common/navbar/navbar.module';
 import { ListOperatorComponent } from './common/list-operator/list-operator.component';
 import { FormsModule } from '@angular/forms';
 import { MiddlewareserviceService } from './common/service/middlewareservice.service';
+import { HttpHandlerInterceptor } from './common/service/http-handler.interceptor';
 
 @NgModule({
   declarations: [AppComponent, ListOperatorComponent],
@@ -21,6 +22,12 @@ import { MiddlewareserviceService } from './common/service/middlewareservice.ser
     FormsModule,
   ],
   providers: [
+
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass:HttpHandlerInterceptor,
+      multi:true
+    },
     
     {
       provide: APP_INITIALIZER,
